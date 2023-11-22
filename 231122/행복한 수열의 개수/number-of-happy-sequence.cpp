ec4479bb2bb68de402d3fc2e913a9a401572993d
sein <71018440+dslov89@users.noreply.github.com>
2023-11-22 21:06:11 +0900

    Title: 행복한 수열의 개수, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231122/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 231122/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -3,43 +3,43 @@ using namespace std;
 int main() {
     int n,m;
     cin>>n>>m;
-    int arr[n+1][n+1]={};
+    int arr[n][n]={};
     for (int i=0;i<n;i++){
         for (int j=0;j<n;j++){
             cin>>arr[i][j];
         }
     }
     int total=0;
-    int num=0,cnt=1;
+    int num=0,cnt=0;
     for (int i=0;i<n;i++){
+        num=arr[i][0];
         for (int j=0;j<n;j++){
            if(num==arr[i][j]){
             cnt++;
            } else{
             num=arr[i][j];
            }
-           if(cnt>=m){
+           if(cnt==m){
             total++;
             break;
            }
         }
-        num=0;
-        cnt=1;
+        cnt=0;
     }
      for (int i=0;i<n;i++){
+        num=arr[0][i];
         for (int j=0;j<n;j++){
            if(num==arr[j][i]){
             cnt++;
            } else{
             num=arr[j][i];
            }
-           if(cnt>=m){
+           if(cnt==m){
             total++;
             break;
            }
         }
-        num=0;
-        cnt=1;
+        cnt=0;
     }
     cout<<total;
     return 0;
