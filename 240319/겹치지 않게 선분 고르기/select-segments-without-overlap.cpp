247a17b7a96b57fff938836e4ee0b92787e44dc7
sein <71018440+dslov89@users.noreply.github.com>
2024-03-19 16:40:57 +0900

    Title: 겹치지 않게 선분 고르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240319/겹치지 않게 선분 고르기/select-segments-without-overlap.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n;
+int arr[1000]={};
+vector<int> v;
+int total=0,maxN=0;
+
+void Choose(int cnt){
+    if(cnt == n){
+        for (int i=0;i<1000;i++){
+            if(arr[i]>1){
+                return ;
+            }
+        }
+        if(total>maxN){
+            maxN=total;
+        }
+        return ; 
+    }
+
+    for (int i=v[cnt];i<v[cnt+1];i++){
+        arr[i]++;
+    }
+    total++;
+    Choose(cnt+1);
+    total--;
+    for (int i=v[cnt];i<v[cnt+1];i++){
+        arr[i]--;
+    }
+
+    Choose(cnt+1);
+
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int a,b;
+        cin>>a>>b;
+        v.push_back(a);
+        v.push_back(b);
+    }
+    Choose(0);
+    cout<<maxN;
+    return 0;
+}
