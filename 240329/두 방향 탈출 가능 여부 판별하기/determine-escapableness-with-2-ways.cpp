8f43450dc473da96f2342e747452321a6b93c11b
sein <71018440+dslov89@users.noreply.github.com>
2024-03-29 18:34:49 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 1096ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240329/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int n,m;
+int arr[101][101];
+int visit[101][101]={};
+
+//오른쪽, 아래
+int dx[2]={0,1};
+int dy[2]={1,0};
+
+int answer = 0;
+
+bool CanGo(int x,int y){
+    if((x>=0&&x<n&&y>=0&&y<m)&&arr[x][y]){
+      return true;
+    }
+    return false;
+}
+
+void DFS(int x, int y){
+    for(int i=0;i<2;i++){
+        int x1 = x+dx[i];
+        int y1 = y+dy[i];
+        if(CanGo(x1,y1)){
+            visit[x1][y1]=1;
+            DFS(x1,y1);
+        }
+    }
+    if(x==(n-1)&&y==(m-1)){
+        answer = 1;
+    }
+}
+
+int main() {
+    cin>>n>>m;
+    for(int i=0;i<n;i++){
+        for (int j=0;j<m;j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    DFS(0,0);
+
+    cout<<answer;
+    return 0;
+}
