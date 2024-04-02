5112baf6ba0dd264d38c01c7d123fa3fa1f98670
sein <71018440+dslov89@users.noreply.github.com>
2024-04-02 17:45:37 +0900

    Title: 갈 수 있는 곳들, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240402/갈 수 있는 곳들/places-can-go.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int n,k;
+int arr[100][100];
+queue<pair<int,int>> q;
+int visit[100][100]={};
+int dx[4]={0,1,0,-1};
+int dy[4]={1,0,-1,0};
+
+bool isOkay(int x,int y){
+    if(x>=0&&x<n&&y>=0&&y<n){
+        if(!visit[x][y]&&!arr[x][y]){
+            return true;
+        }
+    }
+    return false;
+}
+
+void BFS(){
+    while(!q.empty()){
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        
+        for(int i=0;i<4;i++){
+            int x1 = x+dx[i];
+            int y1 = y+dy[i];
+
+            if(isOkay(x1,y1)){
+                visit[x1][y1]=1;
+                q.push(make_pair(x1,y1));
+            }
+        }
+    }
+}
+
+int main() {
+    cin>>n>>k;
+    for(int i=0;i<n;i++){
+        for (int j=0;j<n;j++){
+            cin>>arr[i][j];
+        }
+    }
+    int r,c;
+    for (int i=0;i<k;i++){
+        cin>>r>>c;
+        r--;
+        c--;
+        q.push(make_pair(r,c));
+        BFS();
+    }
+    int answer=0;
+    for(int i=0;i<n;i++){
+        for (int j=0;j<n;j++){
+            if(visit[i][j]) answer++;
+        }
+    }
+    cout<<answer;
+    return 0;
+}
