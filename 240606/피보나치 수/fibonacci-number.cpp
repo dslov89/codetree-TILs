d028d72b8a28a316cede603424eaf15073c69e50
sein <71018440+dslov89@users.noreply.github.com>
2024-06-06 07:53:52 +0900

    Title: 피보나치 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240606/피보나치 수/fibonacci-number.cpp
+++ 240606/피보나치 수/fibonacci-number.cpp
@@ -2,19 +2,30 @@
 
 using namespace std;
 int n;
-int arr[46]={};
+int arr[47]={};
 
-void Fibo(){
-    arr[0]=1;
-    arr[1]=1;
-    for (int i=2;i<n;i++){
-        arr[i]=arr[i-2]+arr[i-1];
+// void Fibo(){
+//     arr[0]=1;
+//     arr[1]=1;
+//     for (int i=2;i<n;i++){
+//         arr[i]=arr[i-2]+arr[i-1];
+//     }
+// }
+
+int Fibo(int k){
+    if(arr[k]==0){
+        if(k==1||k==2){
+            return arr[k]= 1;
+        }
+        return arr[k]= Fibo(k-1)+Fibo(k-2);
     }
+    return arr[k];
 }
 
 int main() {
     cin>>n;
-    Fibo();
-    cout<<arr[n-1];
+    // Fibo();
+    // cout<<arr[n-1];
+    cout<<Fibo(n);
     return 0;
 }
